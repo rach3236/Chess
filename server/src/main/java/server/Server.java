f31a59faad3e14e5d452ccfff1bc70a60ae865a5
Rachel <rachelmay3236@gmail.com>
2025-10-09 09:07:25 -0600

    got some registration working

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,6 +1,10 @@
 package server;
 
+import com.google.gson.Gson;
 import io.javalin.*;
+import io.javalin.http.Context;
+
+import java.util.Map;
 
 public class Server {
 
@@ -13,9 +17,18 @@ public class Server {
 
         //since the db doesn't exist yet this will help the tests
         my_server.delete("db", ctx -> ctx.result("{}"));
+        my_server.post("user", ctx -> register(ctx));
 
-        my_server.post("user", ctx -> ctx.result("{ \"username\":\"\", \"authToken\":\"\"}"));
 
+    }
+
+    private void register(Context ctx) {
+
+        var serializer = new Gson();
+        var request = serializer.fromJson(ctx.body(), Map.class);
+        // call service to register
+        var response = Map.of("username", request.get("username"), "authToken", "xyz");
+        ctx.result(serializer.toJson(response));
     }
 
     public int run(int desiredPort) {
